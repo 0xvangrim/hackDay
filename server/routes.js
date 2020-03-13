d33c109dd8313675447dd4202d2ec5e4cb505f88
Kenny Chung <kenny.chung@appliedtechnology.se>
2020-03-13 12:44:09 +0100

    searches by date and in the beginnings of searching by headlines and all

--- server/routes.js
+++ server/routes.js
@@ -8,37 +8,42 @@ dotenv.config()
 const apiKey = new NewsAPI(process.env.APIKEY)
 
 router.post('/search', async (req, res) => {
-  const { query, language } = req.body;
-  const data = await fetchHeadLines(query, language)
+  const { query, language, fromDate, toDate } = req.body;
+  const data = await fetchHeadLines(query, language, fromDate, toDate)
   res.json(data)
+
 })
 
-const fetchHeadLines = async (query, language) => {
+router.post('/search-all', async (req, res) => {
+  const { query, language, fromDate, toDate } = req.body;
+  const data = await fetchEverything(query, language, fromDate, toDate)
+  res.json(data)
+
+})
+
+const fetchHeadLines = async (query, language, fromDate, toDate) => {
   const response = await apiKey.v2.topHeadlines(
     {
       q: query,
       language: language.toString(),
+      sortBy: 'publishedAt',
+      from: fromDate,
+      to: toDate,
     }
   )
   return response
 }
-const fetchEverything = async (query, language) => {
+const fetchEverything = async (query, language, fromDate, toDate) => {
   const response = await apiKey.v2.everything(
     {
       q: query,
-      language: language.toString()
+      language: language.toString(),
+      sortBy: 'publishedAt',
+      from: fromDate,
+      to: toDate,
     }
   )
   return response
 }
-
-//fetchEverything()
-
-const fetchSources = async () => {
-  const response = await apiKey.v2.sources()
-  console.log(response)
-}
-
-
 
 module.exports = router;
